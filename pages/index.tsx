06811256241e6de6a7e60dc4c176b4636c119cf3
mustafaihssan <mustafa.ihssan@gmail.com>
2021-04-06 17:10:49 +0300

    remove normal css for tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import styled from '@emotion/styled'
 import Head from 'next/head'
 import xw from 'twin.macro'
 
-import styles from '../styles/Home.module.css'
-
 const title = 'My New Blog Support Typescript'
 
 const Button = styled.button(xw`
@@ -15,22 +13,37 @@ const Button = styled.button(xw`
   relative flex justify-center 
   px-4 py-2 text-sm
   font-medium leading-5 text-white 
-  rounded-md w-64
+  rounded-md w-64 mt-5
+`)
+
+const Container = styled.div(xw`
+  flex flex-col justify-center items-center
+  h-screen w-screen
+  p-0 px-2
+`)
+
+const Main = styled.main(xw`
+  flex flex-col justify-center items-center 
+  p-20 px-0 
+`)
+
+const Title = styled.h1(xw`
+  m-0 leading-normal text-6xl
 `)
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<Container>
 			<Head>
 				<title>Create Next App</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>{title}</h1>
+			<Main>
+				<Title>{title}</Title>
 
 				<Button>Emotion + Tailwind +</Button>
-			</main>
-		</div>
+			</Main>
+		</Container>
 	)
 }
