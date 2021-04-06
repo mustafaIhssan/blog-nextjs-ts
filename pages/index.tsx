7d901ce75e5cf2849e48e7ff41a45cd2fb822be8
Mustafa Ihssan A. Naji <mustafa.ihssan@gmail.com>
2021-04-06 14:06:58 +0300

    Emotion + Tailwind (#2)
    * add base emotion
    
    * tailwind + emotion done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import xw from "twin.macro";
+import styled from "@emotion/styled";
 
 const title: string= "My New Blog Support Typescript"
+const Button = styled.button(xw`
+  bg-indigo-600
+  hover:bg-indigo-500
+  focus[outline-none border-indigo-700 ring]
+  active:bg-indigo-700
+  transition duration-150 ease-in-out
+  relative flex justify-center 
+  px-4 py-2 text-sm
+  font-medium leading-5 text-white 
+  rounded-md w-64
+`);
+
 
 export default function Home() {
   return (
@@ -15,6 +29,9 @@ export default function Home() {
         <h1 className={styles.title}>
           {title}
         </h1>
+
+              <Button>Emotion + Tailwind</Button>
+
       </main>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      ...colors,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  xwind: {
+    mode: "objectstyles",
+  },
 }
